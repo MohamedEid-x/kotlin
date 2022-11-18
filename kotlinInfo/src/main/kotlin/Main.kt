74170d2a577e5ad2bf0e52b5099958a3df67745c
Mohamed Eid <meid89817@gmail.com>
2022-11-18 17:45:05 +0200

    Initial commit

---
+++ kotlinInfo/src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    sum:Double=0.0
+    vaule:Double=0.0
+    possitiveNumber:Boolean=true
+    while(possitiveNumber)
+        println("Enter positive number to get sum")
+        vaule=readline()
+    println(vaule)
+
+}
